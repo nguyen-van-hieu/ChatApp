0f292e6357f96bd4203fbc8f8f69f07f80825ac6
hieu <nvhieu99999@gmail.com>
2019-03-12 11:15:34 +0700

    login

--- App/App.js
+++ App/App.js
@@ -1,30 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity, StyleSheet, TextInput, View,Text} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+
+
+export default class App extends Component{
+  state={
+    phone:'',
+    name:''
+  }
+  handleChange =key=>val =>{
+    this.setState({
+      [key] :val
+    })
+  }
+  onSubmitFrom=()=>{
+    alert(this.state.name)
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+          <TouchableOpacity >
+        <Text style={styles.logo}>^.^CHAT^.^</Text>
+      </TouchableOpacity>
+    
+      <TextInput
+      placeholder='Phone Number'
+      keyboardType='number-pad'
+      style={styles.input }
+      value={this.state.phone}
+      onChangeText={this.handleChange('phone')}
+      />
+      <TextInput
+      placeholder='Name'
+      style={styles.input}
+      value={this.state.name}
+      onChangeText={this.handleChange('name')}
+      />
+      <TouchableOpacity onPress={this.onSubmitFrom}>
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
+      
       </View>
     );
   }
@@ -36,15 +53,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  input:{
+    padding :10 ,
+    borderWidth:1,
+    borderColor:'#f7c602',
+    width :'90%',
+    marginBottom:10,
+    borderRadius :50
+
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  btnText :{
+    borderWidth:2,
+    borderColor:'#f7c602',
+    borderRadius :30,
+    padding :10 ,
+    fontSize :20
+    
+
   },
+  logo:{
+    marginBottom:100,
+    /* borderWidth:2,
+    borderColor:'#f7c602', */
+    borderRadius :50,
+    padding :10 ,
+    color :'#f7c602',
+    fontSize :40
+
+  }
+
 });
